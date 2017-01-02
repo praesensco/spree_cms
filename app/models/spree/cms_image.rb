8b8c0719ce60f725191b9efc82e5afd8fc69f69a
Paweł <pawel@itideas.pl>
2017-01-02 13:27:59 +0100

    File path fix

--- app/models/spree/cms_image.rb
+++ app/models/spree/cms_image.rb
@@ -25,7 +25,7 @@ module Spree
       attachment_definitions[:attachment][:url] = ':s3_alias_url'
       attachment_definitions[:attachment][:s3_host_alias] = Rails.application.secrets.attachments_host || Rails.application.secrets.cdn_host
     else
-      attachment_definitions[:attachment][:path] = "/public/media/cmsimage/:id/:style/:basename.:extension"
+      attachment_definitions[:attachment][:path] = "#{Rails.root.to_s}/public/media/cmsimage/:id/:style/:basename.:extension"
       attachment_definitions[:attachment][:url] = "http://#{Rails.application.secrets.cdn_host}/media/cmsimage/:id/:style/:basename.:extension"
     end
 
