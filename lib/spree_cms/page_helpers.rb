7f86601034d5226ff187ba3bef402a5db83a7304
Paweł <pawel@itideas.pl>
2017-03-30 13:54:49 +0200

    Add cms_asset helper

--- lib/spree_cms/page_helpers.rb
+++ lib/spree_cms/page_helpers.rb
@@ -9,6 +9,10 @@ module SpreeCms
       lookup_context.find_all(partial_path, [], true).any?
     end
 
+    def cms_asset(image_definition, size)
+      Spree::CmsImage.where(id: image_definition[:id]).first
+    end
+
     def cms_image(image_definition, size)
       asset = Spree::CmsImage.where(id: image_definition[:id]).first
       return image_definition[:url] if asset.blank?
